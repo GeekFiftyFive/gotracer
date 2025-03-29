e964e7a7b2d98a35fc28df26337f8dab5df0e674
GeekFiftyFive <amylouisepellatt@gmail.com>
2025-03-29 20:43:52 +0000

    Allow dialectric materials to sometimes reflect

--- main.go
+++ main.go
@@ -15,16 +15,19 @@ func main() {
 	materialGround := material.NewLambertian(color.NewColor(0.8, 0.8, 0.0))
 	materialCenter := material.NewLambertian(color.NewColor(0.1, 0.2, 0.5))
 	materialLeft := material.NewDialectric(1.5)
+	materialBubble := material.NewDialectric(1.0 / 1.5)
 	materialRight := material.NewMetal(color.NewColor(0.8, 0.6, 0.2), 1.0)
 
 	sphereGround := sphere.NewSphere(vector.NewVector3(0.0, -100.5, -1.0), 100.0, &materialGround)
 	sphereCenter := sphere.NewSphere(vector.NewVector3(0.0, 0.0, -1.2), 0.5, &materialCenter)
 	sphereLeft := sphere.NewSphere(vector.NewVector3(-1.0, 0.0, -1.0), 0.5, &materialLeft)
+	sphereBubble := sphere.NewSphere(vector.NewVector3(-1.0, 0.0, -1.0), 0.4, &materialBubble)
 	sphereRight := sphere.NewSphere(vector.NewVector3(1.0, 0.0, -1.0), 0.5, &materialRight)
 
 	world.Add(&sphereGround)
 	world.Add(&sphereCenter)
 	world.Add(&sphereLeft)
+	world.Add(&sphereBubble)
 	world.Add(&sphereRight)
 
 	cam := camera.Camera{AspectRatio: 16.0 / 9.0, ImageWidth: 400, SamplesPerPixel: 100, MaxDepth: 50}
--- material/dialectric.go
+++ material/dialectric.go
@@ -3,7 +3,9 @@ package material
 import (
 	"gotracer/color"
 	"gotracer/ray"
+	"gotracer/utils"
 	"gotracer/vector"
+	"math"
 )
 
 type Dialectric struct {
@@ -25,7 +27,25 @@ func (d *Dialectric) Scatter(in ray.Ray, rec *HitRecord) (isScatter bool, scatte
 	}
 
 	unitDirection := in.Direction().UnitVector()
-	refracted := vector.Refract(unitDirection, rec.Normal, ri)
-	scattered = ray.NewRay(rec.P, refracted)
+	cosTheta := math.Min(unitDirection.MultiplyFloat(-1).Dot(rec.Normal), 1)
+	sinTheta := math.Sqrt(1.0 - cosTheta*cosTheta)
+
+	cannotRefract := ri*sinTheta > 1.0
+	var direction vector.Vector3
+
+	if cannotRefract || reflectance(cosTheta, ri) > utils.RandomFloat() {
+		direction = vector.Reflect(unitDirection, rec.Normal)
+	} else {
+		direction = vector.Refract(unitDirection, rec.Normal, ri)
+	}
+
+	scattered = ray.NewRay(rec.P, direction)
 	return
+}
+
+func reflectance(cosine, refractionIndex float64) float64 {
+	// Use Schlick's approximation for reflectance
+	r0 := (1 - refractionIndex) / (1 + refractionIndex)
+	r0 *= r0
+	return r0 + (1-r0)*math.Pow((1-cosine), 5)
 }
